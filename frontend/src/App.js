a6c2a22e2c6fc48228e4f6ac0f1b6162ecf99350
mohammedelmala <mala_moh@msn.com>
2020-12-19 11:04:42 +0400

    commit
    add bootstrap and css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 
 const App = () => {
   return (
     <>
-      Welcome in Proshop
+      <Header />
+      <main>
+        Welcome in Proshop
+      </main>
+
+      <Footer />
     </>
   );
 }
